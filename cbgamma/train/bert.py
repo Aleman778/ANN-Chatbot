dec49c795972a7beb814238cfe195944c8509552
Alexander Mennborg <alexander.mennborg@gmail.com>
2019-12-18 18:31:36 +0100

    Created data transformers for vectorize/ tokenize the dataset

--- cbgamma/train/bert.py
+++ cbgamma/train/bert.py
@@ -10,41 +10,6 @@ from cbgamma.datasets import AmazonReviews
 import logging
 logging.basicConfig(level=logging.INFO)
 
-
-class BertTransform:
-    """Tokenizes the input sequence in the dataset using the BertTokenizer.
-    The BERT model takes in tensor of indexed tokens."""
-    
-    def __init__(self, max_len, pretrained_weights):
-        """Creates a BERT transformer that tokenizes the input text for
-        use in the BERT model. This requires pretrained weights."""
-        # The pretrained BERT tokenizer.
-        self.tokenizer = BertTokenizer.from_pretrained(pretrained_weights)
-        self.max_len = max_len;
-        
-
-    def __call__(self, dataset):
-        tokens = []
-        for text in dataset:
-            padded_text = text + " <pad>"*(self.max_len)
-            
-            # Splits the input text into separate tokens.
-            tokenized_text = self.tokenizer.tokenize(padded_text)
-
-            # Prepends a classifier token and appends a separator token.
-            indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text[0:self.max_len])
-
-            # Converts the textual tokens into indices.
-            indexed_tokens = self.tokenizer.build_inputs_with_special_tokens(indexed_tokens)
-
-            # Appends the indexed tokens to the output tokens for entire dataset
-            tokens.append(indexed_tokens)
-        return tokens
-
-class DataToTensor:
-    def __call__(self, data, target):
-        return torch.tensor(data), target
-    
     
 class BertSentiment:
     """Bert Sentment class is used to train the BERT encoder model
--- cbgamma/transforms/transforms.py
+++ cbgamma/transforms/transforms.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-__all__ = ["ToTfidf"]
+__all__ = ["ToTfidf", "BertTransform", "DataToTensor"]
 
 
 class ToTfidf(TfidfVectorizer):
@@ -29,3 +29,42 @@ class ToTfidf(TfidfVectorizer):
         self.fit = False
         return torch.from_numpy(np.array(data.todense())).type(torch.FloatTensor)
 
+
+
+class BertTransform:
+    """Tokenizes the input sequence in the dataset using the BertTokenizer.
+    The BERT model takes in tensor of indexed tokens."""
+    
+    def __init__(self, max_len, pretrained_weights):
+        """Creates a BERT transformer that tokenizes the input text for
+        use in the BERT model. This requires pretrained weights."""
+        # The pretrained BERT tokenizer.
+        self.tokenizer = BertTokenizer.from_pretrained(pretrained_weights)
+        self.max_len = max_len;
+        
+
+    def __call__(self, dataset):
+        tokens = []
+        for text in dataset:
+            padded_text = text + " <pad>"*(self.max_len)
+            
+            # Splits the input text into separate tokens.
+            tokenized_text = self.tokenizer.tokenize(padded_text)
+
+            # Prepends a classifier token and appends a separator token.
+            indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text[0:self.max_len])
+
+            # Converts the textual tokens into indices.
+            indexed_tokens = self.tokenizer.build_inputs_with_special_tokens(indexed_tokens)
+
+            # Appends the indexed tokens to the output tokens for entire dataset
+            tokens.append(indexed_tokens)
+        return tokens
+
+    
+class DataToTensor:
+    """Similar to ToTensor transformer but only
+    transforms the data and leaves the target unchanged."""
+    def __call__(self, data, target):
+        return torch.tensor(data), target
+    
